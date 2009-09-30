53fb0aca1dc9df23ba521ef4a9f22502e05941bc
abayer <abayer@dev.java.net>
2009-09-30 13:16:57 +0000

    Committing up-to-date setenv plugin

--- src/main/java/hudson/plugins/setenv/SetEnvBuildWrapper.java
+++ src/main/java/hudson/plugins/setenv/SetEnvBuildWrapper.java
@@ -5,20 +5,27 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Properties;
+import java.io.ByteArrayInputStream;
+import java.io.StringReader;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
-
+import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
 
-
+import hudson.EnvVars;
+import hudson.Extension;
 import hudson.Launcher;
-import hudson.model.Build;
 import hudson.model.Descriptor;
 import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
 import hudson.plugins.setenv.Messages;
 import hudson.tasks.BuildWrapper;
-
+import hudson.tasks.BuildWrapperDescriptor;
 
 /**
  * This plugin will set user-configured environment variables. 
@@ -26,131 +33,89 @@ import hudson.tasks.BuildWrapper;
  * @author Takeshi Wakasugi
  */
 public class SetEnvBuildWrapper extends BuildWrapper {
+    private String localVarText;
+
+    @DataBoundConstructor
+    public SetEnvBuildWrapper(String localVarText){
+        this.localVarText = localVarText;
+    }
+    
+    public String getLocalVarText() {
+        return localVarText;
+    }
+    
+    public void setLocalVarText(String localVarText) {
+        this.localVarText = localVarText;
+    }
+    
+    public Map<String,String> getLocalVarMap() {
+        Map<String,String> map = new HashMap<String,String>(0);
+        try {
+            for (Entry<Object,Object> entry : load(localVarText).entrySet()) {
+                map.put(entry.getKey().toString(), entry.getValue().toString());
+            }
+        }
+        catch (IOException e) {
+            // Not sure what to do here?
+        }
+
+        return map;
+    }
+
+    private Properties load(String properties) throws IOException {
+        Properties p = new Properties();
+        try {
+            p.load(new StringReader(properties));
+        } catch (NoSuchMethodError e) {
+            // load(Reader) method is only available on JDK6.
+            // this fall back version doesn't work correctly with non-ASCII characters,
+            // but there's no other easy ways out it seems.
+            p.load(new ByteArrayInputStream(properties.getBytes()));
+        }
+        return p;
+    }
+
+    /*    public class EnvironmentImpl extends BuildWrapper.Environment{
+        public void buildEnvVars(Map<String, String> env) {
+            // To set local-configured value
+            Map<String,String> localVarMap = getLocalVarMap();
+            if(localVarMap != null)env.putAll(localVarMap);
+        }
+        }*/
+
+    @Override
+    public Environment setUp(AbstractBuild build, Launcher launcher,
+                             BuildListener listener) throws IOException, InterruptedException {
+        class EnvironmentImpl extends BuildWrapper.Environment {
+            public void buildEnvVars(Map<String, String> env) {
+                // To set local-configured value
+                Map<String,String> localVarMap = getLocalVarMap();
+                EnvVars.resolve(localVarMap);
+                if (localVarMap != null) {
+                    env.putAll(localVarMap);
+                }
+            }
+        }
+        
+        return new EnvironmentImpl();
+    }
+    
     /**
-     * Descriptor should be singleton.
-     */
-    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
-
-	
-	public DescriptorImpl getDescriptor() {
-		return DESCRIPTOR;
-	}
-
-	private String localVarText;
-	
-	public SetEnvBuildWrapper(String localVarText){
-		this.localVarText = localVarText;
-	}
-
-	public String getLocalVarText() {
-		return localVarText;
-	}
-
-	public void setLocalVarText(String localVarText) {
-		this.localVarText = localVarText;
-	}
-
-	public Map<String,String> getGlobalVarMap(){
-		return getVarMap(getDescriptor().getGlobalVarText());
-	}
-
-	public Map<String,String> getLocalVarMap(){
-		return getVarMap(localVarText);
-	}
-
-	private Map<String,String> getVarMap(String varText){
-		Map<String,String> map = new HashMap<String,String>(0);
-		if(varText == null)return null;
-		String[] lines = varText.split("\\r?\\n");
-		for(String line : lines){
-			String[] words = line.split("=");
-			if(words.length >= 2){
-				// NOTE: words[2..n] will be ignored.
-				if(words[0] != null && words[1] != null)
-					map.put(words[0].trim(), words[1].trim());
-			}
-		}
-		return map;
-	}
-
-	private void expandVarMap(Map<String,String> varMap){
-		Iterator<Entry<String,String>> iter = varMap.entrySet().iterator();
-		while(iter.hasNext()){
-			Entry<String,String>entry = iter.next(); 
-			entry.setValue(expandVariable(entry.getValue(), varMap));
-		}
-	}
-	
-	private String expandVariable(String value, Map<String,String> varMap){
-		int beginPoint = value.indexOf("${");
-		if(beginPoint == -1)return value;
-		int endPoint = value.indexOf("}", beginPoint);
-		if(endPoint == -1)return value;
-		String expandVarName = value.substring(beginPoint+2, endPoint);
-		String expandVarValue = varMap.get(expandVarName);
-		if(expandVarValue==null)expandVarValue="";
-		value = value.substring(0, beginPoint) + expandVarValue + value.substring(endPoint +1);
-		return expandVariable(value, varMap);
-	}
-	
-	
-
-	public class Environment extends BuildWrapper.Environment{
-		public void buildEnvVars(Map<String, String> env) {
-			// To set global-configured value
-			Map<String,String> globalVarMap = getGlobalVarMap();
-			if(globalVarMap != null)env.putAll(globalVarMap);
-			// To set local-configured value
-			Map<String,String> localVarMap = getLocalVarMap();
-			if(localVarMap != null)env.putAll(localVarMap);
-			expandVarMap(env);
-		}
-	}
-
-	public Environment setUp(AbstractBuild build, Launcher launcher,
-			BuildListener listener) throws IOException, InterruptedException {
-		Environment env = new Environment();
-		return env;
-	}
-
-	public Environment setUp(Build build, Launcher launcher,
-			BuildListener listener) throws IOException, InterruptedException {
-		return setUp((AbstractBuild)build,launcher,listener);
-	}
-	
-	/**
      * Descriptor for {@link SetEnvBuildWrapper}. Used as a singleton.
      * The class is marked as public so that it can be accessed from views.
      */
-    public static final class DescriptorImpl extends Descriptor<BuildWrapper> {
-    	private String globalVarText;
-
-        DescriptorImpl() {
-            super(SetEnvBuildWrapper.class);
-            load();
-        }
-
+    @Extension
+    public static final class DescriptorImpl extends BuildWrapperDescriptor {
+        @Override
         public String getDisplayName() {
             return Messages.SetEnvBuildWrapper_DisplayName(); 
         }
 
-        public boolean configure(StaplerRequest req) throws FormException {
-        	globalVarText = req.getParameter("setenv.global_var_text"); 
-        	save();
-            return super.configure(req);
+        @Override
+        public boolean isApplicable(AbstractProject item) {
+            return true;
         }
 
-        public SetEnvBuildWrapper newInstance(StaplerRequest req) throws FormException {
-            return new SetEnvBuildWrapper(req.getParameter("setenv.local_var_text"));
-        }
-
-		public String getGlobalVarText() {
-			return globalVarText;
-		}
-
-		public void setGlobalVarText(String globalVarText) {
-			this.globalVarText = globalVarText;
-		}
     }
 
 }
